51a901c74f2df38137a24789bc23c7c47d7769ec
Matthew Scott Conroy <matthewconroy@pop-os.localdomain>
2025-02-23 17:56:53 -0500

    Adding casino and dna code

---
+++ Unit-02/lab-03/dna/Bioinformatics.java
@@ -0,0 +1,41 @@
+package dna;
+
+public class Bioinformatics {
+    
+    public static ArrayList<String> DNAToCodons(String dna){
+        ArrayList<String> codons = new ArrayList<String>();
+    
+        //Do the work of converting the dna parameter to a collection of codons as type 
+        //ArrayList<String>
+
+        return codons;
+    }
+
+    public static String CodonToAminoAcid(String codon){
+        String aminoAcid;
+
+        // Convert the codon parameter into an aminoAcid of type string
+
+        return aminoAcid;
+    }
+
+    public static ArrayList<String> dna_to_amino_acid(String dna){
+        String aminoAcid;
+
+        //Take the dna parameter and turn it into an aminoAcid of type String
+
+        return aminoAcid;
+    }
+
+    public static boolean is_match(ArrayList<String> amino_seq1, ArrayList<String> amino_seq2){
+        boolean isMatch;
+
+        //Compare the two input parameters to see if they are the same. Set isMatch appropriately.
+
+        return isMatch;
+    }
+
+    public static void main (String[] args){
+       
+    }
+}
